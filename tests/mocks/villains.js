6b3d30495c018e3b364a3ee64eadb203817ffb71
Ikponmwosa <otasowieik@gmail.com>
2021-12-23 16:31:04 -0500

    updated controller, added information to test files

---
+++ tests/mocks/villains.js
@@ -0,0 +1,27 @@
+const allVillains = [{
+  name: 'Captain Hook',
+  movie: 'Peter Pan',
+  slug: 'captain-hook',
+}, {
+  name: 'Cruella de Vil',
+  movie: 'One Hundred and One Dalmatians',
+  slug: 'cruella-de-vil',
+}, {
+  name: 'Gaston',
+  movie: 'Beauty and the Beast',
+  slug: 'gaston',
+}]
+
+const singleVillain = {
+  name: 'Captain Hook',
+  movie: 'Peter Pan',
+  slug: 'captain-hook',
+}
+
+const newVillain = {
+  name: 'Ursula',
+  movie: 'The Little Mermaid',
+  slug: 'ursula',
+}
+
+module.exports = { allVillains, singleVillain, newVillain }
