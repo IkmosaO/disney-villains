51e58038118f214d718f1d6ba357e72726d7a597
Ikponmwosa <otasowieik@gmail.com>
2021-12-23 17:20:34 -0500

    updated tests

---
+++ tests/controllers/villains.test.js
@@ -0,0 +1,71 @@
+/* eslint-disable max-len */
+const chai = require('chai')
+const sinon = require('sinon')
+const sinonChai = require('sinon-chai')
+const models = require('../../models')
+const {
+  afterEach, before, beforeEach, describe, it
+} = require('mocha')
+const { allVillains, singleVillain, newVillain } = require('../mocks/villains')
+const { getAllVillains, getVillainBySlug, saveNewVillains } = require('../../controller/villains')
+
+chai.use(sinonChai)
+const { expect } = chai
+
+describe('controllers - villains', () => {
+  let response
+  let sandbox
+  let stubbedCreate
+  let stubbedFindOne
+  let stubbedFindAll
+  let stubbedSend
+  let stubbedSendStatus
+  let stubbedStatus
+  let stubbedStatusDotSend
+
+  before(() => {
+    sandbox = sinon.createSandbox()
+
+    stubbedFindAll = sandbox.stub(models.Villains, 'findAll')
+    stubbedFindOne = sandbox.stub(models.Villains, 'findOne')
+    stubbedCreate = sandbox.stub(models.Villains, 'create')
+
+    stubbedSend = sandbox.stub()
+    stubbedSendStatus = sandbox.stub()
+    stubbedStatusDotSend = sandbox.stub()
+    stubbedStatus = sandbox.stub()
+
+    response = {
+      send: stubbedSend,
+      sendStatus: stubbedSendStatus,
+      status: stubbedStatus,
+    }
+  })
+
+  beforeEach(() => {
+    stubbedStatus.returns({ send: stubbedStatusDotSend })
+  })
+
+  afterEach(() => {
+    sandbox.reset()
+  })
+  describe('getAllVillains', () => {
+    it('retrieves a list of villains from the database and calls response.send() with the list', async () => {
+      stubbedFindAll.returns(allVillains)
+
+      await getAllVillains({}, response)
+
+      expect(stubbedFindAll).to.have.callCount(1)
+      expect(stubbedSend).to.have.been.calledWith(allVillains)
+    })
+    it('returns a 500 status when an error occurs retrieving all the villains', async () => {
+      stubbedFindAll.throws('ERROR!')
+
+      await getAllVillains({}, response)
+
+      expect(stubbedFindAll).to.have.callCount(1)
+      expect(stubbedStatus).to.have.been.calledWith(500)
+      expect(stubbedStatusDotSend).to.have.been.calledWith('Unable to retrieve all villains')
+    })
+  })
+})
